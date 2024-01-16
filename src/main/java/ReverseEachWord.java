b13768df2740075662194373b8bf2f63f1c90faa
Merve Gurleyen <mervegurleyeen@gmail.com>
2024-01-15 22:27:38 -0800

    String reverse each word

---
+++ src/main/java/ReverseEachWord.java
@@ -0,0 +1,21 @@
+public class ReverseEachWord {
+
+    public static void main(String[] args) {
+
+        String str="Merve Gurleyen";
+        String[] words = str.split(" ");
+        String reversedString = "";
+        for (int i = 0; i < words.length; i++)
+        {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length()-1; j >= 0; j--)
+            {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+        System.out.println(str);
+        System.out.println(reversedString);
+    }
+}
